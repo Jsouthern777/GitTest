7adcad6310ba64964981ebddc500dcc92e926629
Jsouthern777 <southernjj21@gcc.edu>
2024-02-19 15:08:22 -0500

    for my home was never on the ground

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello me");
         System.out.println("How hi are you");
+        System.out.println("I got no roots");
     }
 }
